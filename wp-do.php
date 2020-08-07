9594f82ca643721f523355227a28b40cd5523ed2
Nuno Bispo <contact@bispo-mobile.net>
2020-08-07 14:55:21 +0200

    Updated README.txt

--- wp-do.php
+++ wp-do.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       WP Digital Ocean
  * Plugin URI:        https://github.com/nunombispo/WP-DigitalOcean
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       This plugin provides an interface to the Digital Ocean APIs
  * Version:           1.0.0
  * Author:            Nuno Bispo
  * Author URI:        bispo-mobile.net
