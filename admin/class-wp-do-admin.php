42b0dc8edd45c50966101de843f1f4427226b8d3
Nuno Bispo <contact@bispo-mobile.net>
2020-08-07 12:22:47 +0200

    Admin menu and Settings

--- admin/class-wp-do-admin.php
+++ admin/class-wp-do-admin.php
@@ -102,11 +102,8 @@ class Wp_Do_Admin {
 	}
 
     /**
-     *
-     * admin/class-wp-cbf-admin.php - Don't add this
-     *
-     **/
-
+     * Register admin menus and settings link
+     */
     public function addPluginAdminMenu() {
         //add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
         add_menu_page(  $this->plugin_name, 'WP Digital Ocean', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminDashboard' ));
@@ -126,6 +123,5 @@ class Wp_Do_Admin {
             '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
         );
         return array_merge(  $settings_link, $links );
-
     }
 }
